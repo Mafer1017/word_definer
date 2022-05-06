65cb8f6148a40af7659d8b243c1957e90d238649
Marcus <marcusferreira23@gmail.com>
2022-05-05 23:27:05 -0700

    update first test. add correct code for passing test. failing anyways.

--- lib/word.rb
+++ lib/word.rb
@@ -1,1 +1,24 @@
 class Word
+  attr_reader :id, :word
+
+  @@words = {}
+  @@total_rows = 0
+
+  def initialize(word, id)
+    @word = word
+    @id = id || @@total_rows += 1
+  end
+
+  def self.all
+    @@words.values()
+  end
+
+  def save
+    @@words[self.id] = Word.new(self.word, self.id)
+  end
+
+  def self.clear
+    @@words = {}
+    @@total_rows = 0
+  end
+end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -2,14 +2,17 @@ require 'rspec'
 require 'word'
 
 describe '#Word' do
+
   before(:each) do
     Word.clear()
   end
+
   describe('.all') do
     it("returns an empty array when there are no words") do
       expect(Word.all).to(eq([]))
     end
   end
+
   describe('#save') do
     it("saves a word") do
       word = Word.new("dog", nil)
