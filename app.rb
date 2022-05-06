869aa0103fc8cd6ed5940142627b29972071fdc6
Marcus <marcusferreira23@gmail.com>
2022-05-06 12:27:34 -0700

    add routes and views for editing word name and returning to word list.

--- app.rb
+++ app.rb
@@ -10,6 +10,35 @@ get('/') do
 end
 
 get ('/words') do
-  @words = Words.all
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words/new') do
+  erb(:new_word)
+end
+
+post('/words') do
+  word = params[:inputted_word]
+  word = Word.new(word, nil)
+  word.save()
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
+
+get('/words/:id/edit') do
+  @word = Word.find(params[:id].to_i())
+  erb(:edit_word)
+end
+
+patch('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.update(params[:word])
+  @words = Word.all
   erb(:words)
 end
