6af63e22c755044c8d164c122c624f0930a113eb
Marcus <marcusferreira23@gmail.com>
2022-05-06 10:01:12 -0700

    Add routes and views for home and words.

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require('sinatra')
 require('sinatra/reloader')
 require('./lib/word')
 require('pry')
-require('./lib/song')
 also_reload('lib/**/*.rb')
+
+get('/') do
+  @words = Word.all
+  erb(:words)
+end
+
+get ('/words') do
+  @words = Words.all
+  erb(:words)
+end
