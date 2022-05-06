306393db77ea63857843b92cb408634792016c11
Marcus <marcusferreira23@gmail.com>
2022-05-05 22:24:19 -0700

    write first test for save functionality.

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,2 +1,22 @@
 require 'rspec'
 require 'word'
+
+describe '#Word' do
+  before(:each) do
+    Word.clear()
+  end
+  describe('.all') do
+    it("returns an empty array when there are no words") do
+      expect(Word.all).to(eq([]))
+    end
+  end
+  describe('#save') do
+    it("saves a word") do
+      word = Word.new("dog", nil)
+      word.save()
+      word2 = Word.new("cat", nil)
+      word2.save()
+      expect(Word.all).to(eq([word, word2]))
+    end
+  end
+end
