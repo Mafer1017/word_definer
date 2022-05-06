4b1813d8d5a90b77fac3b2057fa12cf48b4585c6
Marcus <marcusferreira23@gmail.com>
2022-05-06 09:29:44 -0700

    add test for delete functionality.

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -61,4 +61,15 @@ describe '#Word' do
     expect(input.word).to(eq("booger"))
     end
   end
+
+  describe('#delete') do
+    it("deletes a word by id") do
+      word = Word.new("dog", nil)
+      word.save()
+      word2 = Word.new("sky", nil)
+      word2.save()
+      word.delete()
+      expect(Word.all).to(eq([word2]))
+    end
+  end
 end
