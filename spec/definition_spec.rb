f69b0d5e6f534f8dfca9676c2f4673bea8bc8648
Marcus <marcusferreira23@gmail.com>
2022-05-06 23:55:16 -0700

    add and pass test for finding definitions for a word.

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -76,4 +76,16 @@ describe '#Definition' do
       expect(Definition.all).to(eq([definition2]))
     end
   end
+
+  describe('.find_by_word') do
+    it("finds definitions for a word") do
+      word2 = Word.new("cat", nil)
+      word2.save
+      definition = Definition.new("animal that is furry", @word.id, nil)
+      definition.save()
+      definition2 = Definition.new("animal that purrs", word2.id , nil)
+      definition2.save()
+      expect(Definition.find_by_word(word2.id)).to(eq([definition2]))
+    end
+  end
 end
